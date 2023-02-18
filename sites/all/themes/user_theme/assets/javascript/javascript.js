f2eaca8c70107f0630364d42c96af2f1a220c64c
Moobbot <ngotam24082001@gmail.com>
2023-02-18 19:36:15 +0700

    format code

---
+++ sites/all/themes/user_theme/assets/javascript/javascript.js
@@ -0,0 +1,7 @@
+(function ($) {
+	'use strict';
+	$(document).ready(function () {});
+	$(document).mouseup(function (e) {});
+	window.addEventListener('mouseover', function (e) {});
+	window.addEventListener('load', function () {});
+})(window.jQuery);
