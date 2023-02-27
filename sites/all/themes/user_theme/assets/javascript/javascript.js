28a74ec5928abe19a9b8e1da585a84d7a6749c99
Moobbot <ngotam24082001@gmail.com>
2023-02-27 20:49:53 +0700

    update new format

--- sites/all/themes/user_theme/assets/javascript/javascript.js
+++ sites/all/themes/user_theme/assets/javascript/javascript.js
@@ -1,7 +1,24 @@
 (function ($) {
 	'use strict';
-	$(document).ready(function () {});
+	$(document).ready(function () {
+		var owl = $('.banner-carousel');
+		owl.owlCarousel({
+			items: 1,
+			loop: true,
+			nav: false,
+			dots: false,
+			autoHeight: true,
+			autoplay: true,
+			autoplayTimeout: 1500,
+			autoplayHoverPause: true,
+		});
+	});
 	$(document).mouseup(function (e) {});
-	window.addEventListener('mouseover', function (e) {});
+	window.addEventListener('mouseover', function (e) {
+		var container = $('name');
+		if (!container.is(e.target) && container.has(e.target).length === 0) {
+			//code here
+		}
+	});
 	window.addEventListener('load', function () {});
 })(window.jQuery);
