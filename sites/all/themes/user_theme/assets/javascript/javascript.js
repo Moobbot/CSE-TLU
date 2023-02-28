302ea1451b4b33c59ed2766e3395e55f30e294df
Moobbot <ngotam24082001@gmail.com>
2023-02-28 09:03:02 +0700

    update footer

--- sites/all/themes/user_theme/assets/javascript/javascript.js
+++ sites/all/themes/user_theme/assets/javascript/javascript.js
@@ -9,8 +9,22 @@
 			dots: false,
 			autoHeight: true,
 			autoplay: true,
-			autoplayTimeout: 2000,  
+			autoplayTimeout: 3000,
 			autoplayHoverPause: true,
+		});
+		var body_width = $('body').width();
+		// 5px scrollbar
+		if (body_width < 988) $('.footer-detail').fadeOut();
+
+		// Footer
+		$('.footer-title').click(function (e) {
+			// $(this).siblings('.footer-detail').toggleClass('open');
+			$(this).siblings('.footer-detail').fadeToggle();
+		});
+		$(window).resize(function () {
+			body_width = $('body').width();
+			if (body_width < 988) $('.footer-detail').fadeOut();
+			else $('.footer-detail').show();
 		});
 	});
 	$(document).mouseup(function (e) {});
